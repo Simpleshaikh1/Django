da35d650a622d05f4b6b44fcde23ce55213692b2
Simpleshaikh1 <58138876+Simpleshaikh1@users.noreply.github.com>
2020-10-27 00:46:47 -0700

    initial commit

---
+++ hangman.py
@@ -0,0 +1,44 @@
+
+# first = "Morsh"
+# last = "Hamedani"
+# full = f"{len(first)} {2 + 2}"
+# print(full)
+# course = "     python programming"
+# print(course.upper())
+# print(course.lower())
+# print(course.title())
+# print(course.lstrip())
+# print(course.find("pro"))
+# print(course.replace("p", "j"))
+# print("pro" in course)
+# print("swift" not in cours
+# print(round(2.9))
+# print(abs(-2.9))
+
+# print(math.ceil(2.2))
+
+# def greet(name):
+# print(f"hi {name} ")
+#  return "...."
+
+
+# print(greet("mosh"))
+
+
+# def increment(number, by=1):
+#  return number + by
+
+
+# print(increment(2, 5))
+
+# def multiply(*numbers):
+total = 1
+   for number in numbers:
+      total *= number
+    return total
+
+# to save information of a user
+def save_user(**user):
+  print (user)
+
+save_user(id=1, name="Johm", age = 22)
